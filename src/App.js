e735931c36dd8f1324f792d239ed0ec766a70778
ZabWP <zabdiel.bronola01@gmail.com>
2025-05-02 12:23:50 -0400

    added routed library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import GameOfLife from "./game";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <GameOfLife />
+      <Router>
+        <Routes>
+          <Route path="/" element={<GameOfLife />} />
+          <Route path="/game" element={<GameOfLife />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
